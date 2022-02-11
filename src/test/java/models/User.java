14899e4bb0f776180bdd89fe41acc61f63ab58a6
felixmaina <karani.maina2010@gmail.com>
2022-02-11 17:51:24 +0300

    genrate equals and hashcode

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
     private  int id;
     private String name;
@@ -26,5 +28,22 @@ public class User {
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(position, user.position) && Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, position, role);
     }
 }
