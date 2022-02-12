bb904090df44218514ea90356303e2d7c0adbb9c
felixmaina <karani.maina2010@gmail.com>
2022-02-13 01:30:03 +0300

    override the add method

--- src/main/java/dao/Sql2oUsersDao.java
+++ src/main/java/dao/Sql2oUsersDao.java
@@ -1,11 +1,29 @@
 package dao;
 
+import models.User;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class Sql2oUsersDao  implements  UsersDao{
 private Sql2o sql2o;
     public Sql2oUsersDao(Sql2o sql2o) {
         this.sql2o = sql2o;
+    }
+    @Override
+    public void add(User user) {
+        try (Connection con=sql2o.open()){
+            String sql ="INSERT INTO staff (name,position,staff_role) VALUES (:name,:position,:role) ";
+            int id=(int) con.createQuery(sql,true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+
+
+        }catch (Sql2oException e){
+            System.out.println(e);
+        }
+
     }
 
 }
