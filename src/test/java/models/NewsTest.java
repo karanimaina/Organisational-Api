269adb3f71fef1218a6f8ebe1cf8c9b3acc033bc
felixmaina <karani.maina2010@gmail.com>
2022-02-12 10:29:32 +0300

    Generate a constructor

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,18 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NewsTest {
+    @Test
+    public void News_NewsInstantiatesCorrectly() {
+    News news = setupNews();
+    assertTrue(news instanceof News);
+    }
+
+    private News setupNews (){
+        return new News("Position advert","new intern required",1);
+    }
 
 }
