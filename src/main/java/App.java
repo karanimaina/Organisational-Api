3f3c9320f774df448ca72f9356ee839dfbb807df
felixmaina <karani.maina2010@gmail.com>
2022-02-13 05:18:25 +0300

    gets  alll  news , departments and users saved

---
+++ src/main/java/App.java
@@ -0,0 +1,52 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        Sql2oNewsDao sql2oNewsDao;
+        Sql2oUsersDao sql2oUsersDao;
+        Sql2oDepartmentsDao sql2oDepartmentsDao;
+        Connection conn;
+        Gson gson = new Gson();
+        staticFileLocation("/public");
+        //uncomment the line below to run locally
+        //String connectionString = "jdbc:postgresql://localhost:5432/organisational_news_portal";
+        //uncomment the line below to run locally,change the following line of code to your credentials
+        //Sql2o sql2o = new Sql2o(connectionString, "wangui", "33234159");
+
+        //the two lines below are used when using heroku but if you want to run locally comment them
+        String connectionString = "jdbc:postgresql://ec2-50-17-21-170.compute-1.amazonaws.com:5432/d8b8ehu0safpui"; //!
+        Sql2o sql2o = new Sql2o(connectionString, "mihpivzxyyqmlv", "5b4f9d76874ad368465a325b3993140263c6d254771908c3d283842d54fcad11");
+
+        sql2oDepartmentsDao=new Sql2oDepartmentsDao(sql2o);
+        sql2oNewsDao=new Sql2oNewsDao(sql2o);
+        sql2oUsersDao=new Sql2oUsersDao(sql2o);
+        conn=sql2o.open();
+         //resds  users , news ,departments
+        get("/users", "application/json", (request, response) -> {
+
+            if(sql2oDepartmentsDao.getAll().size() > 0){
+                return gson.toJson(sql2oUsersDao.getAll());
+            }
+            else {
+                return "{\"message\":\"I'm sorry, but no users are currently listed in the database.\"}";
+            }
+        });
+
+
